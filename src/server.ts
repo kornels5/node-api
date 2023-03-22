b5a91f8ddda91cf53ea8d79fe7184b3cc33c913e
kornels <kornel.sroka@picodi.com>
2023-03-22 19:45:26 +0100

    Error handling

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,10 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.status(200);
-  res.json({ message: "hello" });
+app.get("/", (req, res, next) => {
+  setTimeout(() => {
+    next(new Error("error"));
+  }, 1000);
 });
 
 app.use("/api", protect, router);
@@ -21,4 +22,13 @@ app.use("/api", protect, router);
 app.post("/user", createNewUser);
 app.post("/signin", signin);
 
+app.use((err, req, res, next) => {
+  if (err.type === "auth") {
+    res.status(401).json({ message: "unauthorized" });
+  } else if (err.type === "input") {
+    res.status(400).json({ message: "Bad request" });
+  } else {
+    res.status(500).json({ message: "500 Internal server Error" });
+  }
+});
 export default app;
